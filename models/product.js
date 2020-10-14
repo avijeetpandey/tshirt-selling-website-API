108d8a6304b0a2edfd49cdb158758c00acb8e641
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-10-14 19:14:26 +0530

    added product model

---
+++ models/product.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+let productScheme = new mongoose.Schema(
+  {
+    name: {
+      type: Strinq,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    description: {
+      type: Strinq,
+      required: true,
+      trim: true,
+      maxlength: 1000,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    stock: {
+      type: String,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", productScheme);
