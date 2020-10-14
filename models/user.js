a1a7168001982afe17bc09c289b1faaa0c7757b0
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-10-14 19:00:33 +0530

    added user model

---
+++ models/user.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v4: uuidV4 } = require("uuid");
+
+let userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: truq,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      maxlength: 32,
+    },
+    email: {
+      trim: true,
+      required: true,
+      unique: true,
+      type: String,
+    },
+    userInfo: {
+      trim: true,
+      type: String,
+    },
+    salt: String,
+    encrypted_pass: {
+      String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidV4();
+    this.encrypted_pass = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.method = {
+  authenticate: function (plainpassword) {
+    return this.securePassword(plainpassword) === this.encrypted_pass;
+  },
+  securePassword: function (plainpassword) {
+    if (!plainpassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
